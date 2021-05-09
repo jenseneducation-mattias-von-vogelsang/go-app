1eea505cc912fb19e75720f8b1df2bfc3c34c041
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2021-05-09 21:57:27 +0200

    fixade så att login söker igenom databasen efter användare och sedan skickar token om det finns

--- app.go
+++ app.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"time"
 
 	jwt "github.com/form3tech-oss/jwt-go"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	jwtware "github.com/gofiber/jwt/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -17,33 +19,32 @@ const collectionName = "users"
 const port = ":3000"
 
 type User struct {
-	_id       string `json:"id,omitempty"`
-	UserName  string `json:"username,omitempty"`
-	FirstName string `json:"firstname,omitempty"`
-	LastName  string `json:"lastname,omitempty"`
-	Email     string `json:"email,omitempty"`
-	Password  string `json:"password,omitempty`
-	Age       int    `json:"age,omitempty"`
+	_id       string `json:"id,omitempty" binding:"required"`
+	FirstName string `json:"firstname,omitempty" binding:"required"`
+	LastName  string `json:"lastname,omitempty" binding:"required"`
+	Email     string `json:"email,omitempty" binding:"required"`
+	Password  string `json:"password,omitempty" binding:"required"`
+	Age       int    `json:"age,omitempty" binding:"required"`
 }
 
 func main() {
 	app := fiber.New()
-
-	//TODO: lös login med datat i mongoDB
-	app.Post("/login", login)
+	app.Use(logger.New())
 
 	app.Get("/", accessible)
+
+	app.Post("/login", login)
+
+	//TODO: kolla om användaren redan finns i databasen, isåfall meddela
+	app.Post("/user", postUser)
 
 	app.Use(jwtware.New(jwtware.Config{
 		SigningKey: []byte("secret"),
 	}))
 
-	app.Get("/user/:id?", getUser)
+	app.Get("/user/:id", getUser)
 
 	app.Get("/users", getUsers)
-
-	//TODO: lägg till jwttoken vid signup
-	app.Post("/user", postUser)
 
 	app.Put("/user/:id", updateUser)
 
@@ -62,6 +63,7 @@ func getUser(c *fiber.Ctx) error {
 
 	var filter bson.M = bson.M{}
 
+	//TODO: slå mot databas
 	if c.Params("id") != "" {
 		id := c.Params("id")
 		objID, _ := primitive.ObjectIDFromHex(id)
@@ -71,6 +73,7 @@ func getUser(c *fiber.Ctx) error {
 	var results []bson.M
 	cur, err := collection.Find(context.Background(), filter)
 	defer cur.Close(context.Background())
+	fmt.Println(context.Background())
 
 	if err != nil {
 		return c.Status(500).Send([]byte(err.Error()))
@@ -175,19 +178,35 @@ func login(c *fiber.Ctx) error {
 	user := c.FormValue("user")
 	pass := c.FormValue("pass")
 
-	// Throws Unauthorized error
-	if user != "john" || pass != "doe" {
+	collection, err := getMongoDbCollection(dbName, collectionName)
+	if err != nil {
+		return c.Status(500).Send([]byte(err.Error()))
+	}
+
+	var filter bson.M = bson.M{}
+
+	//TODO: slå mot databas
+	if c.Params("id") != "" {
+		filter = bson.M{"user": user, "pass": pass}
+	}
+
+	var result User
+
+	if err = collection.FindOne(context.Background(), filter).Decode(&result); err != nil {
 		return c.SendStatus(fiber.StatusUnauthorized)
 	}
+
+	fmt.Println(result)
 
 	// Create token
 	token := jwt.New(jwt.SigningMethodHS256)
 
 	// Set claims
 	claims := token.Claims.(jwt.MapClaims)
-	claims["name"] = "John Doe"
+
+	claims["name"] = result.FirstName
 	claims["admin"] = true
-	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
 
 	// Generate encoded token and send it as response.
 	t, err := token.SignedString([]byte("secret"))
